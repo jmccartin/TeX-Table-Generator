7b00e19e1cfd7f44927c0c50868ee55e909cc0d2
Joseph McCartin <joseph.mccartin@gmail.com>
2014-01-31 18:29:01 +0100

    Optimised tablecreator

--- TableCreator/tablecreator.py
+++ TableCreator/tablecreator.py
@@ -37,56 +37,48 @@ class TableCreator(object):
                         for key,value in dictionary.items():
                                 process_unc_dict[key] = value
 
-                # Arrange the keys of the dictionary into the order defined in 'datasets' specified in the config
-                channels = {}
+                # Create an ordered dictionary with associated values to the keys that were ordered in the previous step
+                sorted_processes = collections.OrderedDict()
+                sorted_uncerts = collections.OrderedDict()
                 i = 0
                 for dataset in self.datasets:
                         for element in process_dict.keys():
-                                # Regexp the name of the dataset given the base name that appears in the config
-                                name = re.search('('+str(dataset)+'\w+)', element)
-                                if name:
-                                        channels[name.groups()[0]] = i
-                                        i += 1
+                                matched_name = re.search('('+str(dataset)+'\w+)', element)
+                                if matched_name:
+                                        translated_id = self.config_reader.get_latex_dataset_name(str(matched_name.groups()[0]))
+                                        sorted_processes[translated_id] = process_dict[matched_name.groups()[0]]
+                                        sorted_uncerts[translated_id] = process_unc_dict[matched_name.groups()[0]]
 
-                # Create an ordered dictionary with associated values to the keys that were ordered in the previous step
-                sorted_processes = collections.OrderedDict()
-                for key in sorted(process_dict, key=channels.get):
-                        # Look up the latex translation for the given dataset name from the config
-                        name = self.config_reader.get_latex_dataset_name(str(key))
-                        sorted_processes[name] = process_dict[key]
-
-                # Create an ordered dictionary with associated uncertainty values to the keys that were ordered in the previous step
-                sorted_uncerts = collections.OrderedDict()
-                for key in sorted(process_unc_dict, key=channels.get):
-                        # Look up the latex translation for the given dataset name from the config
-                        name = self.config_reader.get_latex_dataset_name(str(key))
-                        sorted_uncerts[name] = process_unc_dict[key]
-
-                # Map the values to a list so a total can be appended to it later
-                events = map(list, sorted_processes.values())
-                event_uncerts = map(list, sorted_uncerts.values())
+                sorted_processes_2 = collections.OrderedDict()
+                for i in range(len(sorted_processes.keys())):
+                        if (i == 0):
+                                sorted_processes_2[sorted_processes.keys()[1]] = sorted_processes.values()[1]
+                        elif (i == 1):
+                                sorted_processes_2[sorted_processes.keys()[0]] = sorted_processes.values()[0]
+                        else:
+                                sorted_processes_2[sorted_processes.keys()[i]] = sorted_processes.values()[i]
 
                 # Calculate the total events passed and associated uncertainties for each row
                 total_mc = []
                 total_mc_uncert = []
-                for j in range(len(events[0])):
+                for j in range(len(sorted_processes.values()[0])):
                         events_passed_total = 0
                         uncertainty_total = 0
-                        for i in range(len(events)):
-                                events_passed_total += events[i][j]
-                                uncertainty_total += event_uncerts[i][j]
+                        for i in range(len(sorted_processes.values())):
+                                events_passed_total += sorted_processes.values()[i][j]
+                                uncertainty_total += sorted_uncerts.values()[i][j]
                         total_mc.append(events_passed_total)
                         total_mc_uncert.append(uncertainty_total)
-                # Append the totals back to the mapped list
-                events.append(total_mc)
-                event_uncerts.append(total_mc_uncert)
+
+                # Append the totals back to the dict
+                sorted_processes['Total MC'] = total_mc
+                sorted_uncerts['Total MC'] = total_mc_uncert
 
                 # Write the names of the process + channel to each column
-                self.table_file.write(' & '.join(str(x) for x in sorted_processes.keys()) + ' & ')
-                self.table_file.write(r'Total MC \\' + '\n')
+                self.table_file.write(' & '.join(str(x) for x in sorted_processes.keys()) + r' \\ ' + '\n')
                 # Write the values + uncertainty to each column
-                for i in range(len(events[0])):
-                        self.table_file.write('\t' + ' & '.join(str(int(x[i])) + r' \pm ' + str(int(y[i])) for x, y in zip(events, event_uncerts)) + r' \\' + '\n')
+                for i in range(len(sorted_processes.values()[0])):
+                        self.table_file.write('\t' + ' & '.join(str(int(x[i])) + r' \pm ' + str(int(y[i])) for x, y in zip(sorted_processes.values(), sorted_uncerts.values())) + r' \\' + '\n')
 
                 # Write the closing code to the table
                 self.table_file.write('\t' + r'\hline' + '\n')
